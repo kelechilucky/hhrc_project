65f2bc9ee537ca348bdd934bcb453621d653c751
Ikeolu Kelechi Lucky <kelechiikeolu@gmail.com>
2025-03-14 11:13:47 +0100

    Feat: Started creating app

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+   <div className="flex justify-center items-center ">
+     <h1 className='text-[200px]' >hey you</h1>
+   </div>
+  )
+}
+
+export default Header
